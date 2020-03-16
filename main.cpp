bfdd6ee7c7edca6e97179df8ce17312be981a0df
huajian1069 <huajian1069@gmail.com>
2020-03-16 18:00:38 +0100

    add label with text on GUI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include "window.h"
 #include <QApplication>
 #include <QPushButton>
-#include "window.h"
+
 int main(int argc, char **argv){
     QApplication app(argc, argv);    
     Window window;
--- popup.cpp
+++ popup.cpp
@@ -1,21 +1,28 @@
 #include "popup.h"
 #include <QProgressBar>
 #include <QSlider>
+#include <QLabel>
 
 PopUp::PopUp(QWidget *parent) : QWidget(parent)
 {
-    setFixedSize(200, 80);
+    setFixedSize(230, 90);
     // create a progressor bar
     QProgressBar *progressBar = new QProgressBar(this);
     progressBar->setRange(0, 100);
     progressBar->setValue(0);
-    progressBar->setGeometry(10, 10, 180, 30);
+    progressBar->setGeometry(10, 10, 210, 30);
 
     // create a slider
     QSlider *slider = new QSlider(this);
     slider->setOrientation(Qt::Horizontal);
     slider->setRange(0, 100);
     slider->setValue(0);
-    slider->setGeometry(10, 40, 180, 30);
+    slider->setGeometry(10, 40, 210, 30);
     QObject::connect(slider, SIGNAL(valueChanged(int)), progressBar, SLOT(setValue(int)));
+
+    //create a label
+    QLabel *label = new QLabel(this);
+    label->setText("Have fun play around with this GUI");
+    label->setGeometry(10, 70, 210, 20);
+    label->show();
 }
